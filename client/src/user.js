b8c8f2b3e01869d61a1a093b1cc232a5e04a4fab
nofh202 <nofh202@gmail.com>
2022-02-19 17:33:18 +0200

    בדגדג

--- client/src/user.js
+++ client/src/user.js
@@ -1,14 +1,17 @@
 class User extends React.Component {
     constructor(props) {
         super(props);
-      
-      
+    
     }
-
     
     render() {
-        return (
-          null
+        return ( 
+            <tr>
+                <td>{this.props.user.id}</td>
+                <td>{this.props.user.full_name}</td>
+                <td>{this.props.user.email}</td>
+                <td>{this.props.user.status}</td>
+            </tr>
         );
     }
 }
--- client/src/users.js
+++ client/src/users.js
@@ -1,10 +1,16 @@
+import User from './user.js';
+
 class Users extends React.Component {
     constructor(props) {
         super(props);
         this.state={
             users = []
         }
-      
+        this.handle_click = this.handle_click.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ [event.target.name]: event.target.value });
     }
 
     async componentDidMount() {
@@ -15,35 +21,143 @@ class Users extends React.Component {
         this.setState({ users: users });
     }
 
-
     async fetch_users() {
         const cook = this.props.cookie;
-        const res = await fetch('/api/messages', {
+        const res = await fetch('/api/users', {
             method: "GET",
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': cook
             }
         });
-
-        if (res.status == 200) {
-
-            const messages = await res.json();
-            const reverse_messages = messages.reverse();
-            const n_messages = reverse_messages.slice(0, 6);
-            this.update_list(n_messages);
-
-        }
-        else {
-            const err = await res.text();
-            alert(err);
-
-        }
     }
 
+    async handle_click(id)
+	{
+		const response = await fetch('/api/user/' + id , {method:'GET'});
+		if ( response.status == 200 )
+		{
+			const user = await response.json();
+            const buttonDel = document.querySelector('delete')
+            const buttonSus = document.querySelector('suspend')
+            const buttonAct = document.querySelector('activate')
+            if (user.status == 'deleted')
+            {
+                buttonDel.setState({isDisabled: true});
+                buttonSus.setState({isDisabled: true});
+                buttonAct.setState({isDisabled: true});
+            }
+                
+            if (user.status=='suspended')
+            {
+                buttonDel.setState({isDisabled: false});
+                buttonSus.setState({isDisabled: true});
+                buttonAct.setState({isDisabled: false});
+            }
+            if (user.status=='active')
+            {
+                buttonDel.setState({isDisabled: true});
+                buttonSus.setState({isDisabled: true});
+                buttonAct.setState({isDisabled: false});
+            }
+            //const users = await this.fetch_users();
+            
+			//this.update_list(users);		  
+		}
+		else 
+		{
+		  const err = await response.text();
+		  alert( err );
+		}
+	}
+
+    async handle_Approve_Clicked(id)
+    {
+        event.preventDefault();
+        const response = await fetch('/api/user/approve/'+ id , {
+            method: 'PUT'
+            });
+            if ( response.status == 200 )
+                {
+                    const users = await response.json();
+                    update_list(users);
+                    window.location.reload(false);	
+                }
+            else 
+            {
+                const err = await response.text();
+                alert( err );
+            }
+
+    }
+    
+    async handle_Suspend_Clicked(id)
+    {
+        const response = await fetch('/api/user/suspend/'+id, {
+            method: 'PUT'
+            });
+            if ( response.status == 200 )
+                {
+                    const users = await response.json();
+                    update_list(users);
+                    window.location.reload(false);	
+                }
+            else 
+            {
+                const err = await response.text();
+                alert( err );
+            }
+    }
+
+    async handle_Activate_Clicked(id)
+    {
+        const response = await fetch('/api/user/restore/'+id, {
+            method: 'PUT'
+            });
+            if ( response.status == 200 )
+                {
+                    const users = await response.json();
+                    update_list(users);
+                    window.location.reload(false);	
+                }
+            else 
+            {
+                const err = await response.text();
+                alert( err );
+            }
+    }
     render() {
         return (
-          null
+            <div style="overflow-x:auto;">
+                <table>
+                <tr>
+                    <th>id</th>
+                    <th>Full Name</th>
+                    <th>Email</th>
+                    <th>status</th>
+                </tr>
+                    {this.props.users.map( (item,index) => { return <User user={item} key = {index} /> }  ) }
+                </table>
+                <br></br>
+                
+                    <button type="approve" className="btn" 
+                        disabled
+                        onClick={this.handle_Approve_Clicked.bind(this)}>
+                        approve user
+                    </button>
+                <br></br>
+                    <button type="suspend" className="btn" 
+                        disabled 
+                        onClick={this.handle_Suspend_Clicked.bind(this)}>
+                            suspend user
+                    </button>
+                <br></br>
+                    <button type="activate" className="btn" 
+                        disabled
+                        onClick={this.handle_Activate_Clicked.bind(this)}>
+                            activate user
+                    </button>
+            </div>
         );
     }
 }
